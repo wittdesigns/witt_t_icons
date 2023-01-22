fe3b536309523db0e581fe07288810125b2e39da
wittdesigns <113542953+wittdesigns@users.noreply.github.com>
2023-01-21 22:34:20 -0500

    script files added

--- js/main.js
+++ js/main.js
@@ -12,7 +12,16 @@ the document is the DOM - the web page and all of its elements
 
 //step 1 - make the connection to the elements you want to interact with
 //const jpgGraphic = document.querySelector("#bitmap");
-const vectorGraphic = document.querySelector("#newlogo");
+const icon001 = document.querySelector("#vinil");
+const icon002 = document.querySelector("#oldtape");
+const icon003 = document.querySelector("#playbt001");
+const icon004 = document.querySelector("#oldtape");
+const icon005 = document.querySelector("#pausebutton");
+const icon006 = document.querySelector("#nexttrack");
+const icon007 = document.querySelector("#oldtape");
+const icon008 = document.querySelector("#oldtape");
+const icon009 = document.querySelector("#radio_new");
+const icon010 = document.querySelector("#volumecontrol");
 //here calls the elements, selectors
 
 
@@ -23,7 +32,15 @@ function logthisID(){
 
 
 //step 2 - decide how you want the user to interact with the object you created in step 1
-vectorGraphic.addEventListener('click', logthisID);
+icon001.addEventListener('click', logthisID);
+icon002.addEventListener('click', logthisID);
+icon003.addEventListener('click', logthisID);
+icon004.addEventListener('click', logthisID);
+icon005.addEventListener('click', logthisID);
+icon007.addEventListener('click', logthisID);
+icon008.addEventListener('click', logthisID);
+icon009.addEventListener('click', logthisID);
+icon010.addEventListener('click', logthisID);
 //here its the items calling the function to be applied
 
 
